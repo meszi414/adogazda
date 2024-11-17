1c4248510034c9565be3a61b9a2a45219fc647d8
meszi414 <meszi414@gmail.com>
2024-11-17 11:49:45 +0100

    képek, facebook

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -3,26 +3,26 @@ import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
   <Section
-    title="Üdvözlünk az AdóGazda honlapján!"
+    title="Üdvözlünk az Adó-TanGazdagság Bt. honlapján!"
     description="Több mint 20 év szakmai tapasztalattal, mérlegképes könyvelőként, adótanácsadóként és pénzügy-számvitel szakos közgazdászként segítem ügyfeleimet a biztos pénzügyi háttér megteremtésében. Fontos számunkra a precizitás és a naprakész tudás, hogy vállalkozásod számára a legjobb megoldásokat kínálhassuk."
   >
     <VerticalFeatureRow
       title="Teljes körű könyvelés és adótanácsadás"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature.svg"
+      image="/assets/images/acc1.jpg"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
       title="Jogszabályi változások követése, adóoptimalizálás"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature2.svg"
+      image="/assets/images/tax1.jpg"
       imageAlt="Second feature alt text"
       reverse
     />
     <VerticalFeatureRow
       title="Céges átvilágítás a pénzügyi átláthatóságért"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature3.svg"
+      image="/assets/images/audit1.jpg"
       imageAlt="Third feature alt text"
     />
   </Section>
